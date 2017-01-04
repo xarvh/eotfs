71daabb1864281f6abab8b37341b63e03946552e
xarvh <francesco.orsenigo@gmail.com>
2017-01-04 23:04:35 +1100

    Threads scraping

---
+++ scraping/NOTES.md
@@ -0,0 +1,11 @@
+
+
+
+
+
+From https://www.rpnation.com/search/?&q=emperors%20of%20the%20fading%20suns&type=forums_topic&page=8&nodes=55
+use `$('ol[data-role="resultsContents"] li.ipsPad h2 > div a').toArray().map((e) => e.href)`
+
+
+
+
---
+++ scraping/scrape.py
@@ -0,0 +1,23 @@
+import scrapy
+
+
+class EotfsSpider(scrapy.Spider):
+    name = "eotfs"
+    start_urls = [
+        'https://www.rpnation.com/topic/49741-emperors-of-the-fading-suns-emperors-of-the-fading-suns-chapter-6-scene-17-renovations/'
+    ]
+
+    def parse(self, response):
+        threadId = response.url.split("/")[-2]
+        for post in response.css('article'):
+            yield {
+                    'threadId': threadId,
+                    'author': post.css('h3 strong a::text').extract_first(),
+                    'timestamp': post.css('time::attr(datetime)').extract_first(),
+                    'content': post.css('[data-role="commentContent"] > *').extract(),
+            }
+
+#        next_page = response.css('li.next a::attr("href")').extract_first()
+#        if next_page is not None:
+#            next_page = response.urljoin(next_page)
+#            yield scrapy.Request(next_page, callback=self.parse)
